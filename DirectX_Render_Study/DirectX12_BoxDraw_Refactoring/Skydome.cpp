10ac49ce74142ea51754dc573a3e275a12b881f9
NakajimaYuuhi <yuuhi.nakajima.138@gmail.com>
2026-06-26 06:48:12 +0900

    読み込みチェック
    新しいスカイドームは、テクスチャ読み込み失敗

--- DirectX_Render_Study/DirectX12_BoxDraw_Refactoring/Skydome.cpp
+++ DirectX_Render_Study/DirectX12_BoxDraw_Refactoring/Skydome.cpp
@@ -35,13 +35,17 @@ Skydome::Skydome(String _Name)
 
 	//----- Model -----
 	//ModelPath = "Assets/Model/OffensiveIdle.glb";
+	//ModelPath = "Assets/Model/free_-_skybox_anime_sky.glb";
 	ModelPath = "Assets/Model/uploads_files_4381633_GreenExplosion.glb";
+
 	//ステージで使うモデルを一覧にしてどこかでロードしておくといいかも
 
 	CModel* model = GetComponent<CModel>();
 
 
 	auto sharedModel = ModelManager::GetInstance().GetModel(ModelPath);
+
+	
 
 	model->CopyFrom(sharedModel);
 	model->PlayAnimation(0);
